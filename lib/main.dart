404ead607842b18992982539bafc48eb51980779
“cerkesbilal” <bilalkarademir86@gmail.com>
2025-07-10 11:08:10 +0300

    textformfield konusu anlatildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +8,242 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyProject());
+  }
+}
+
+class MyProject extends StatelessWidget {
+  const MyProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("İnput Tekrar"),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: TextFormFieldKullanimi(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TextFormFieldKullanimi extends StatefulWidget {
+  const TextFormFieldKullanimi({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TextFormFieldKullanimi> createState() => _TextFormFieldKullanimiState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TextFormFieldKullanimiState extends State<TextFormFieldKullanimi> {
+  late final String _email, _password, _userName;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          //validate işlemini ne zaman çalıştıracağını belirler
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+            children: [
+              //TextEditingcontroller a ihtiyaç duymaz çünkü onSaved vardır.
+              TextFormField(
+                onSaved: (gelenUserName) {
+                  _userName = gelenUserName!;
+                },
+                //varsayılan değeri tanımlar
+                //initialValue: "bilalkarademir",
+                decoration: InputDecoration(
+                  //hata mesajlarının rengini değiştirir
+                  errorStyle: TextStyle(color: Colors.orange),
+                  labelText: "Kullanıcı Adı",
+                  hintText: "Username",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (girilenUserName) {
+                  if (girilenUserName!.isEmpty) {
+                    return "Kullanıcı adı boş olamaz";
+                  }
+                  if (girilenUserName.length < 4) {
+                    return "4 karakterden az olamaz";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(height: 10),
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+              TextFormField(
+                onSaved: (gelenMail) {
+                  _email = gelenMail!;
+                },
+                //varsayılan değeri tanımlar
+                //initialValue: "bilalkarademir",
+                decoration: InputDecoration(
+                  //hata mesajlarının rengini değiştirir
+                  errorStyle: TextStyle(color: Colors.orange),
+                  labelText: "Email",
+                  hintText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (girilenEmail) {
+                  if (!EmailValidator.validate(girilenEmail!)) {
+                    return "Geçerli bir mail giriniz";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+
+              SizedBox(height: 10),
+
+              TextFormField(
+                onSaved: (gelenSifre) {
+                  _password = gelenSifre!;
+                },
+                //varsayılan değeri tanımlar
+                //initialValue: "bilalkarademir",
+                decoration: InputDecoration(
+                  //hata mesajlarının rengini değiştirir
+                  errorStyle: TextStyle(color: Colors.orange),
+                  labelText: "Şifre",
+                  hintText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (girilenPass) {
+                  if (girilenPass!.isEmpty) {
+                    return "Şifre boş olamaz";
+                  }
+                  if (girilenPass.length < 6) {
+                    return "şifre en az 6 karakter olmalı";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(height: 10),
+
+              SizedBox(
+                width: 180,
+                height: 55,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green.shade400,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Colors.green, width: 3),
+                    ),
+                  ),
+                  onPressed: () {
+                    //validate tamamlandı mı kontrol etmek için
+                    bool _isValidate = _formKey.currentState!.validate();
+                    if (_isValidate) {
+                      //textformfield dan gelen verileri kaydetme işlemi
+                      _formKey.currentState!.save();
+                      String result =
+                          "username: $_userName\nemail:$_email\nşifre:$_password";
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: Colors.pink,
+                          content: Text(
+                            result,
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                      );
+                      //save işlemi olduktan sonra textformfieldları temizler
+                      _formKey.currentState!.reset();
+                    }
+                  },
+                  child: Text("Onayla"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldWidgetKullanimi extends StatefulWidget {
+  const TextFieldWidgetKullanimi({super.key});
+
+  @override
+  State<TextFieldWidgetKullanimi> createState() =>
+      _TextFieldWidgetKullanimiState();
+}
+
+class _TextFieldWidgetKullanimiState extends State<TextFieldWidgetKullanimi> {
+  late TextEditingController _emailController;
+  late FocusNode _focusNode;
+  Color myFillColor = Colors.white;
+  int maxLineCount = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _focusNode = FocusNode();
+    _focusNode.addListener(() {
+      setState(() {
+        maxLineCount = _focusNode.hasFocus ? 3 : 1;
+        myFillColor = _focusNode.hasFocus ? Colors.red : Colors.white;
+      });
     });
   }
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: TextField(
+            focusNode: _focusNode,
+            controller: _emailController,
+            maxLines: maxLineCount,
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.done,
+            //autofocus: true,
+            cursorColor: Colors.red,
+            decoration: InputDecoration(
+              labelText: "Username",
+
+              hintText: "Kullanıcı adınızı giriniz",
+              icon: Icon(Icons.add),
+
+              prefix: Icon(Icons.person),
+
+              suffixIcon: Icon(Icons.cancel),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+
+              filled: true,
+              fillColor: myFillColor,
             ),
-          ],
+
+            onChanged: (String gelenDeger) {},
+
+            onSubmitted: (String gelenDeger) {},
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        TextField(),
+      ],
     );
   }
 }
